1faa07adf3a29c9fab323be5ac6f278a94f3a307
trinhphu9872 <trinhphu9872@gmail.com>
2023-03-27 12:13:52 +0700

    Phu/push

--- AdvisorManagement/Middleware/MailServicesMiddleware.cs
+++ AdvisorManagement/Middleware/MailServicesMiddleware.cs
@@ -16,11 +16,31 @@ namespace AdvisorManagement.Middleware
 
     public class MailServicesMiddleware 
     {
-
-        public string MailSend(MailRequest request) {
+        public string MailSend(MailRequest request)
+        {
             var email = new MimeMessage();
             email.From.Add(MailboxAddress.Parse(WebConfigurationManager.AppSettings["EmailSystem"]));
             email.To.Add(MailboxAddress.Parse(request.To));
+            email.Subject = request.Subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = request.Message };
+            var smtp = new SmtpClient();
+            smtp.Connect(WebConfigurationManager.AppSettings["EmailHost"], int.Parse(WebConfigurationManager.AppSettings["EmailPort"]), SecureSocketOptions.StartTls);
+            smtp.Authenticate(WebConfigurationManager.AppSettings["EUserName"], WebConfigurationManager.AppSettings["EPassword"]);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+            return "Ok";
+        }
+
+        public string MailSendMuiltiRequest(MailRequest request, List<string> MultiMail)
+        {
+            var email = new MimeMessage();
+            InternetAddressList listMail = new InternetAddressList();
+            MultiMail.ForEach(mail =>
+            {
+                listMail.Add(new MailboxAddress("user", mail));
+            });
+            email.From.Add(MailboxAddress.Parse(WebConfigurationManager.AppSettings["EmailSystem"]));
+            email.To.AddRange(listMail);
             email.Subject = request.Subject;
             email.Body = new TextPart(TextFormat.Html) { Text = request.Message };
             var smtp = new SmtpClient();
