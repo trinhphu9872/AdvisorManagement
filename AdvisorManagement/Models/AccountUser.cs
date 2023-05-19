0739a3855f9cf000f35898ac3fce44c429b75c47
trinhphu9872 <trinhphu9872@gmail.com>
2023-05-20 00:00:15 +0700

    phu/fix

--- AdvisorManagement/Models/AccountUser.cs
+++ AdvisorManagement/Models/AccountUser.cs
@@ -33,10 +33,8 @@ namespace AdvisorManagement.Models
         public string email { get; set; }
         public Nullable<System.DateTime> create_time { get; set; }
         public Nullable<System.DateTime> update_time { get; set; }
-
         [NotMapped]
         public HttpPostedFileBase ImageUpload { get; set; }
-
         public virtual Role Role { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Advisor> Advisor { get; set; }
